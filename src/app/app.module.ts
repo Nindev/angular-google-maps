e996a12f01c4dffafe0aa465ff4580008fd1c2ea
DSallee <dsallee.dev@gmail.com>
2018-03-25 21:05:36 +0200

    Deploy AGM Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, ApplicationRef } from '@angular/core';
+import { AgmCoreModule } from '@agm/core'
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      // please get your own API key here: https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
+      apiKey: 'AIzaSyBTlbtSwaIuj4LwfvnppYHW4UJ1pLA9ikI'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
