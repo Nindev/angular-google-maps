3ccd0f3c46e0fb58fbb91a6cc89ded9933a8da51
DSallee <dsallee.dev@gmail.com>
2018-03-25 22:19:54 +0200

    Map display issue fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  // title: string = 'My First AGM Test Project';
+  title: string = 'My First AGM Test Project';
   lat: number = 51.678418;
   lng: number = 7.809007;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ApplicationRef } from '@angular/core';
-import { AgmCoreModule } from '@agm/core'
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+// tslint:disable-next-line:max-line-length
+import { AgmCoreModule, DataLayerManager, KmlLayerManager, PolygonManager, PolylineManager, CircleManager, InfoWindowManager, MarkerManager, GoogleMapsAPIWrapper } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -10,12 +13,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    FormsModule,
     AgmCoreModule.forRoot({
       // please get your own API key here: https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
       apiKey: 'AIzaSyBTlbtSwaIuj4LwfvnppYHW4UJ1pLA9ikI'
     })
   ],
-  providers: [],
+  providers: [
+    GoogleMapsAPIWrapper,
+    MarkerManager,
+    InfoWindowManager,
+    CircleManager,
+    PolylineManager,
+    PolygonManager,
+    KmlLayerManager,
+    DataLayerManager
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
